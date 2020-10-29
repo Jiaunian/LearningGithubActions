47184e4df1b3ed0d940c28997723670037aaf728
j3dream <rivkaer@qq.com>
2020-10-29 14:02:53 +0800

    feat: [Learning] Github Actions Android持续集成(自动化打包发布)示例

---
+++ example/src/main/java/com/lumotime/learning/action/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.lumotime.learning.action
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 显示当前的包名
+        textview.text = "APP Package: $packageName"
+    }
+}
